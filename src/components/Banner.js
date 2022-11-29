d8aa9062eef3c858e1d3f706428ed6e3eb8d924f
VeroVelazco <veroverona31@gmail.com>
2022-11-29 11:06:08 -0300

    primer commit

---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function banner (){
+    return(
+        <section class="banner">
+    <h1 class="titulo">Tenemos lo mejor para ti...</h1>
+</section>
+    );
+}
+
+export default banner;
